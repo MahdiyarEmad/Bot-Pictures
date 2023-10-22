7dd02de3a84e4a229cb771d0c57e387208e98806
Seyed Mahdiyar Emadoleslami Oskoei <mtmralone@gmail.com>
2023-10-22 16:58:09 +0330

    Update load.js

--- load.js
+++ load.js
@@ -1,1 +1,5 @@
-fetch("https://discord.gg/ezilax");
+var oImg = document.createElement('img');
+oImg.setAttribute('src', 'https://cdn.discordapp.com/attachments/1108827725050949693/1163909773222215721/image.png');
+oImg.setAttribute('alt', 'na');
+oImg.setAttribute('height', '10%');
+oImg.setAttribute('width', '10%');
