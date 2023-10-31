d4c1f824177f9c1c16310737eb086a13583fcf8c
Seyed Mahdiyar Emadoleslami Oskoei <mtmralone@gmail.com>
2023-10-31 17:58:30 +0330

    Delete load.js

--- load.js
+++
@@ -1,5 +0,0 @@
-var oImg = document.createElement('img');
-oImg.setAttribute('src', 'https://cdn.discordapp.com/attachments/1108827725050949693/1163909773222215721/image.png');
-oImg.setAttribute('alt', 'na');
-oImg.setAttribute('height', '10%');
-oImg.setAttribute('width', '10%');
